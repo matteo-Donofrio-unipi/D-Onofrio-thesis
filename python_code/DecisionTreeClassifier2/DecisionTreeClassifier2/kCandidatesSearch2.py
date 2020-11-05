84485da082061b52528d44b565d41aa8a6879dda
matteo-Donofrio-unipi <69981848+matteo-Donofrio-unipi@users.noreply.github.com>
2020-11-05 18:38:50 +0100

    codice + esperimenti finiti

---
+++ python_code/DecisionTreeClassifier2/DecisionTreeClassifier2/kCandidatesSearch2.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import pdist, squareform
+
+
+def runKMeans(dataset,n_clusters):
+
+    #columnsList=list(['idTs','IdCandidate,'startingPosition','M/D']) LISTA DELLE PRIME 4 COLONNE BASE DI DATASET
+    #DA QUESTE DEVO ESTRARRE TUTTE LE SUCCESSIVE
+
+
+    numColumnsDataset=list(dataset.columns)
+    numColumnsDataset=numColumnsDataset[4::]
+    X=dataset[numColumnsDataset]
+    #calcolo kmeans tra candidati
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
+    kmeans.fit(X)
+
+    centroids = kmeans.cluster_centers_
+
+    #per ogni centroide, estraggo medoide (record nel cluster piu vicino al centroide )
+    medoids = list()
+    for label in np.unique(kmeans.labels_):
+        X_cluster = X[kmeans.labels_ == label]
+        X_cluster.reset_index(inplace=True)
+        dist = pdist(X_cluster)
+        index = np.argmin(np.sum(squareform(dist), axis=0))
+        medoids.append(X_cluster.iloc[index]['index'])
+    #medoids = np.array(medoids)
+
+
+    #retituisco gli indici, di dataset, che sono stati scelti
+    return medoids
